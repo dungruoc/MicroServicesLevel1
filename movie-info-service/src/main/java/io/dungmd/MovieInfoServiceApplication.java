8a35cd12782e39d77ff8d1318a28e54d3f60be32
dungruoc <minhdung.dang@gmail.com>
2019-08-25 12:51:36 +0700

    Level2 - 9 Adding timeout to RestTemplate

--- movie-info-service/src/main/java/io/dungmd/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/dungmd/MovieInfoServiceApplication.java
@@ -1,18 +1,25 @@
 package io.dungmd;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class MovieInfoServiceApplication {
 
+    @Value("${api.timeout}")
+    private int apiTimeout;
+
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(apiTimeout);
+        return new RestTemplate(clientHttpRequestFactory);
     }
 
 	public static void main(String[] args) {
